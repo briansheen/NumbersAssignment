0d176383f07c23a50bc62d89949871d94174e9ad
BRIAN SHEEN <bsheen@BRIANs-MacBook-Pro.local>
2017-04-04 17:04:17 -0500

    initial commit

--- src/Scan2.java
+++ src/Scan2.java
@@ -1,10 +1,11 @@
+import java.math.BigDecimal;
 import java.util.Scanner;
 
 public class Scan2 {
     public static void main(String args[]) {
         Scanner s = null;
-        Double operand1 = null;
-        Double operand2 = null;
+        BigDecimal operand1 = null;
+        BigDecimal operand2 = null;
         try {
             s = new Scanner(System.in);
             while (s.hasNext()) {
@@ -14,41 +15,45 @@ public class Scan2 {
                 }
                 if (operand1 == null) {
                     try {
-                        operand1 = Double.valueOf(operator);
+                        operand1 = new BigDecimal(operator);
                     } catch (NumberFormatException e) {
                         System.out.println("invalid input, integer required");
                     }
                 } else if (operand2 == null) {
                     try {
-                        operand2 = Double.valueOf(operator);
+                        operand2 = new BigDecimal(operator);
                     } catch (NumberFormatException e) {
                         System.out.println("invalid input, integer required");
                     }
                 } else {
                     if (operator.equals("*")) {
-                        operand1 = operand1 * operand2;
+                        operand1 = operand1.multiply(operand2);
                         operand2 = null;
                     } else if (operator.equals("/")) {
-                        operand1 = operand1 / operand2;
-                        operand2 = null;
+                        try{
+                            operand1 = operand1.divide(operand2);
+                            operand2 = null;
+                        } catch(ArithmeticException e){
+                            System.out.println("non-terminating decimal");
+                        }
                     } else if (operator.equals("+")) {
-                        operand1 = operand1 + operand2;
+                        operand1 = operand1.add(operand2);
                         operand2 = null;
                     } else if (operator.equals("-")) {
-                        operand1 = operand1 - operand2;
+                        operand1 = operand1.subtract(operand2);
                         operand2 = null;
                     } else if (operator.equals("max")) {
-                        operand1 = Math.max(operand1, operand2);
+                        operand1 = operand1.max(operand2);
                         operand2 = null;
                     } else if (operator.equals("min")) {
-                        operand1 = Math.min(operand1, operand2);
+                        operand1 = operand1.min(operand2);
                         operand2 = null;
                     } else if (operator.equals("pow")) {
-                        operand1 = Math.pow(operand1, operand2);
+                        operand1 = operand1.pow(operand2.intValue());
                         operand2 = null;
-                    } else if (operator.equals("hypot")) {
-                        operand1 = Math.hypot(operand1, operand2);
-                        operand2 = null;
+//                    } else if (operator.equals("hypot")) {
+//                        operand1 = Math.hypot(operand1, operand2);
+//                        operand2 = null;
                     } else {
                         System.out.println("invalid input, '*, /, +, -' required");
                     }
